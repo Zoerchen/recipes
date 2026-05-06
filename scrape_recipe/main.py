f8fe7bacb209bfd988a904fe82775cc3e569c976
Zoerchen <zora.gornik@web.de>
2026-05-06 17:40:45 +0200

    Update main.py

--- scrape_recipe/main.py
+++ scrape_recipe/main.py
@@ -29,7 +29,11 @@ class ScrapeRequest(BaseModel):
 #CORS aktivieren
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://zoerchen.github.io/recipes/"],  # später auf deine GitHub Pages URL einschränken
+    allow_origins=[
+        "https://zoerchen.github.io/recipes",
+        "http://127.0.0.1:5500",  # für lokale Entwicklung
+        "http://localhost:5500"    # für lokale Entwicklung],  # später auf deine GitHub Pages URL einschränken
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
